8455064f09912578c51226c8d06bd66abe02d4c7
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-09-04 11:34:47 -0400

    created initial thought controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,7 @@ const userController = {
 
   getAllUsers(req, res) {
     User.find({})
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
+      .populate({ path: 'thoughts', select: '-__v'})
       .select('-__v')
       .sort({ _id: -1 })
       .then(dbUserData => res.json(dbUserData))
@@ -19,10 +16,7 @@ const userController = {
 
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
+      .populate({ path: 'thoughts', select: '-__v' })
       .select('-__v')
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
