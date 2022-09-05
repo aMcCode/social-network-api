7b1b3f627141e448d98955a18a9745439596afbe
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-09-05 13:49:58 -0400

    corrected user, friend and thought controllers and apis, aligned url params w/ assignment requirements

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,6 @@ const userController = {
     User.find({})
       .populate({ path: 'thoughts', select: '-__v'})
       .select('-__v')
-      .sort({ _id: -1 })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
@@ -15,7 +14,7 @@ const userController = {
   },
 
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
+    User.findOne({ _id: params.userId })
       .populate({ path: 'thoughts', select: '-__v' })
       .select('-__v')
       .then(dbUserData => res.json(dbUserData))
@@ -32,7 +31,7 @@ const userController = {
   },
 
   updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
@@ -44,7 +43,7 @@ const userController = {
   },
 
   deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
+    User.findOneAndDelete({ _id: params.userId })
     .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
@@ -56,7 +55,7 @@ const userController = {
   },
 
   addFriend( req, res) {
-    User.findOneAndUpdate({ _id: req.params.id }, {$push: {friends: req.params.friendId }}, { new: true })
+    User.findOneAndUpdate({ _id: req.params.userId }, {$push: {friends: req.params.friendId }}, { new: true })
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
@@ -68,7 +67,7 @@ const userController = {
   },
 
   delFriend( req, res) {
-    User.findOneAndUpdate({ _id: req.params.id }, {$push: {friends: req.params.friendId }}, { new: true })
+    User.findOneAndUpdate({ _id: req.params.userId }, {$pull: {friends: req.params.friendId }}, { new: true })
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
